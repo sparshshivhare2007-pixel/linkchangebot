2e401d526c6d2a3a44974af5a36e44f7d994ceb4
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-08-27 21:04:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", 32141443))
-API_HASH = os.getenv("API_HASH", "4f34a89257ac316505f5a47b237454cc")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8835310418:AAGrYPEu8j7MQKumNTrsoQAHp-VeVd6ZXYE")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1004364000451))
-OWNER_ID = int(os.getenv("OWNER_ID", 8974535424))
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+DEFAULT_TARGET_ID = int(
+    os.getenv("DEFAULT_TARGET_ID", "-1004364000451")
+)
+
+SESSION_FILE = "session_data.json"
+TARGET_FILE = "target.json"
+USERNAMES_FILE = "usernames.txt"
+
+if not API_ID:
+    raise RuntimeError("API_ID missing in .env")
+
+if not API_HASH:
+    raise RuntimeError("API_HASH missing in .env")
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN missing in .env")
+
+if not OWNER_ID:
+    raise RuntimeError("OWNER_ID missing in .env")
