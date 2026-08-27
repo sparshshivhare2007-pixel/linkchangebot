12d1f9e7da84d2ccb07ccae5ef51bc20efd66db9
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-08-27 20:27:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-OWNER_ID = int(os.getenv("OWNER_ID"))
-SESSION_PATH = os.getenv("SESSION_PATH", "session/owner_session.session")
+API_ID = int(os.getenv("API_ID", 32141443))
+API_HASH = os.getenv("API_HASH", "4f34a89257ac316505f5a47b237454cc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8835310418:AAGrYPEu8j7MQKumNTrsoQAHp-VeVd6ZXYE")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1004364000451))
+OWNER_ID = int(os.getenv("OWNER_ID", 8974535424))
